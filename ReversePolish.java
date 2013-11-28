f7d5642e09c1d461f33936f75b91a7ab4f3ac44e
xtang <xin.tang@epfl.ch>
2013-11-28 21:32:35 +0100

    reverse polish notation

--- ReversePolish.java
+++ ReversePolish.java
@@ -1,5 +1,56 @@
 package Problems;
 
-public class ReversePolish {
+import java.util.Stack;
 
+public class ReversePolish {
+	public int evalRPN(String[] tokens) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        Stack<Integer> stack =  new Stack<Integer>();
+        for(int i=0;i<tokens.length; i++){
+        	if(isInteger(tokens[i]))
+        		stack.push(Integer.parseInt(tokens[i]));
+        	else if(isOperator(tokens[i])){
+        		int num1 = stack.pop();
+        		int num2 = stack.pop();
+        		stack.push(calculate(num1, num2, tokens[i]));
+        	}
+        }
+        
+        return stack.pop();
+    }
+	
+	private boolean isInteger(String s) {
+	    try { 
+	        Integer.parseInt(s); 
+	    } catch(NumberFormatException e) { 
+	        return false; 
+	    }
+	    // only got here if we didn't return false
+	    return true;
+	}
+	
+	private boolean isOperator(String s){
+		if(s.length() == 1){
+			if(s.equals("*") || s.equals("/") || s.equals("-") || s.equals("+"))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	private int calculate(int num1, int num2, String operator){
+		if(operator.equals("*"))
+			return num1*num2;
+		else if(operator.equals("-"))
+			return num2 - num1;
+		else if(operator.equals("/"))
+			return num2 / num1;
+		else if(operator.equals("+"))
+			return num1 + num2;
+		else
+			return 0;
+	}
+	
+	
 }
