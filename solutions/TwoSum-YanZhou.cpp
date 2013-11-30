3f1ff73f02961a798b97c1ad6858acb032b12c04
JoeJoe1989 <josephzhou2011@gmail.com>
2013-11-30 18:57:15 +0100

    add a new file

---
+++ solutions/TwoSum-YanZhou.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> resultVector(2);
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            for (int j = i+1; j < numbers.size(); j++)
+            {
+                if ( (numbers[i] + numbers[j]) == target )
+                {
+                    resultVector[0] = i+1;
+                    resultVector[1] = j+1;
+                    break;
+                }
+            }
+        }
+        return resultVector;
+    }
+};
